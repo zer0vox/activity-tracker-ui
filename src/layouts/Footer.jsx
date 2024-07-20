59cbed967cadbd88868f56bcdc4c832ebe1e9f09
Sumip Chaudhary <sumip780@gmail.com>
2024-07-20 12:23:06 +0545

    added footer

---
+++ src/layouts/Footer.jsx
@@ -0,0 +1,23 @@
+import Logo from "../assets/activity.png"
+import { FaGithubAlt } from "react-icons/fa";
+const Footer = () => {
+  return (
+    <div className="container">
+    <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <p className="col-md-4 mb-0 text-muted">&copy; 2021 Company, Inc</p>
+  
+      <a href="/" className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+       <img src={Logo} alt="" width="30" height="24"/>
+      </a>
+  
+      <ul className="nav col-md-4 justify-content-end">
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-muted"><FaGithubAlt /></a></li>
+       
+      </ul>
+    </footer>
+  </div>
+
+  )
+}
+
+export default Footer
