330fea6078ab9188bf22193ab18da3c26b990bb6
Sumip Chaudhary <sumip780@gmail.com>
2024-07-20 11:57:25 +0545

    linked bootstrap to main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-       
-      </div>
-      <h1>Activity Tracker</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     Hello World
     </>
   )
 }
